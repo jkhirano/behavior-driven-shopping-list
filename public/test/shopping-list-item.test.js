a3b674dda69eeb4c2a4f879be1d52f8a82fd5ea2
phi nguyen <vandevildog@gmail.comgit config --global user.email vandevildog@gmail.comgit commit -m 1st>
2019-08-01 19:02:38 -1000

    firsttest

---
+++ public/test/shopping-list-item.test.js
@@ -0,0 +1,6 @@
+describe('Shopping Item', function() {
+  let foo = new ShoppingListItem();
+  it('ShoppingListItem should be a class', function() {
+    expect(foo).to.be.instanceOf(ShoppingListItem);
+  })
+})
